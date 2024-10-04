e55d7c9d074072539a49118d1baeed61b9d0ddb3
Akshata <idakshatasawant@gmail.com>
2024-10-04 14:43:04 +0530

    new updation

--- Third.java
+++ Third.java
@@ -1,8 +1,9 @@
-public class First{
+public class Third{
 	
 	public static void main(String [] args){
 		
 		System.out.println("Hello Everyone...");
+		System.out.println(" Everyone...");
 		
 	}
 	
